b1f3b6110b1ec67ba45cb26f265e1a1b69e2da57
VeRyM <verym2004@gmail.com>
2023-07-10 21:08:06 +1100

    Cycle add

--- js/script.js
+++ js/script.js
@@ -10,12 +10,27 @@ let personalMovieDB = {
     privat: false,
 };
 
-const questionA = prompt('Один из последних просмотренных фильмов?', ""),
-      questionB = +prompt('На сколько оцените его?', ""),
-      questionC = prompt('Один из последних просмотренных фильмов?', ""),
-      questionD = +prompt('На сколько оцените его?', "");
+Survey: for (let i =0; i <2; i++) {
+    const questionA = prompt('Один из последних просмотренных фильмов?', ""),
+          questionB = prompt('На сколько оцените его?', "");
 
-personalMovieDB.movies[questionA] = questionB;
-personalMovieDB.movies[questionC] = questionD;
+    if (questionA === null || questionB === null || questionB === "" || questionA === "" || questionA.length > 50) {
+        alert("Некорректный ответ(");
+        console.log("Некорректный ответ(");
+        i--;
+        continue Survey; 
+    };
+    personalMovieDB.movies[questionA] = questionB;
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Некорретный ввод количества просмотренных фильмов");
+};
 
 console.log(personalMovieDB);
