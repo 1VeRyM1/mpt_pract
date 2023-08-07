f75d38095fb3a021941ce1cda09d6712b4cc34ad
VeRyM <verym2004@gmail.com>
2023-08-08 07:50:11 +1100

    ClassList

--- js/script.js
+++ js/script.js
@@ -1,73 +1,39 @@
 'use strict'
 
-let personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: true,
-    start: function() {
-        do {
-            this.count = prompt('Сколько фильмов вы уже посмотрели?', "");
-        } while (this.count == null || isNaN(this.count) || this.count.trim() == "")
-        this.count =this.count.trim();
-    },
-    rememberMyFilms: function() {
-        Survey: for (let i =0; i <2; i++) {
-            const questionA = prompt('Один из последних просмотренных фильмов?', ""),
-                  questionB = prompt('На сколько оцените его?', "");
-        
-            if (questionA === null || questionB === null || questionB.trim() === "" || questionA.trim() === "" || questionA.length > 50) {
-                alert("Некорректный ответ(");
-                console.log("Некорректный ответ(");
-                i--;
-                continue Survey; 
-            };
-            this.movies[questionA] = questionB.trim();
-        }
-    },
-    detectPersonalLevel: function() {
-        if (this.count < 10) {
-            alert("Просмотрено довольно мало фильмов");
-        } else if (this.count >= 10 && this.count <= 30) {
-            alert("Вы классический зритель");
-        } else if (this.count > 30) {
-            alert("Вы киноман");
-        } else {
-            alert("Некорретный ввод количества просмотренных фильмов");
-        };
-    },
-    writeYourGenres: function() {
-        for (let i = 0; i < 3; i++) {
-            const genres = prompt(`Ваш любимый жанр под номером ${i+1}`,"");
-            if (genres != null && genres.trim() != "") {
-                this.genres[i] = genres.trim();
-            } else {
-                alert("Введите жанр");
-                i--;
-            };
-        }
-        this.genres.forEach(function(element, index) {
-            console.log(`Любимый жанр #${index + 1} - это ${element}`)
-        });
-    },
-    toggleVisibleMyDB: function() {
-        if (this.privat === false) {
-            this.privat = true;
-        } else {
-            this.privat = false;
-        };
-    },
-    showMyDB: function(mainObj) {
-        if (!mainObj.privat){ 
-            console.log(mainObj)
-        };
-    },
-};
+const btns = document.querySelectorAll("button"),
+      wrapper = document.querySelector('.btn-block');
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(1));
+// console.log(btns[0].classList.add('red'));
+// console.log(btns[0].classList.remove('red'));
+// console.log(btns[0].classList.toggle('blue'));
 
-personalMovieDB.start();
-personalMovieDB.rememberMyFilms();
-personalMovieDB.detectPerson alLevel();
-personalMovieDB.writeYourGenres();
-personalMovieDB.toggleVisibleMyDB();
-personalMovieDB.showMyDB(personalMovieDB);
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
+
+// btns[0].addEventListener('click', () => {
+//     // if (!btns[1].classList.contains('red')) {
+//     //     btns[1].classList.add('red');
+//     // } else {
+//     //     btns[1].classList.remove('red');
+//     // };
+//     btns[1].classList.toggle('red');
+// });
+
+// console.log(btns[0].className); // Устарело!
+
+// wrapper.addEventListener('click', (event) => {
+//     if (event.target && event.target.matches('button.red')) {
+//         console.log("hello")
+//     }
+// });
+
+// const btn = document.createElement("button");
+// btn.classList.add("red");
+// wrapper.append(btn);
+
+
+
+
+
